0d78cc678447b761ce2cea61d8715700ea1fac7d
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2025-03-31 14:39:18 +0530

    Operator and Template lit

---
+++ Episode-5/Operators.js
@@ -0,0 +1,4 @@
+let num = 90;
+// num++
+console.log(num++)
+console.log(++num)
---
+++ Episode-6/templatelit.js
@@ -0,0 +1,10 @@
+//Template String used to concate
+let nan = `React js`
+//String interpolation
+let new_str = `${nan} is a library`
+console.log(new_str)
+
+let n1 = parseInt(Math.random() * 100) + 1;
+let n2 = parseInt(Math.random() * 100) + 1;
+let n3 = n1+n2;
+console.log(`The sum of ${n1} and ${n2} is ${n3}`);
