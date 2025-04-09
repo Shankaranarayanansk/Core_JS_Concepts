186b350866873b06e07c78c934098d36b077fbd2
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2025-04-09 18:37:16 +0530

    addReplaceRemove

---
+++ Episode-19/addRemoveReplace.js
@@ -0,0 +1,19 @@
+let arr = [100,23,45,67,89,90]
+let a = arr.pop();
+console.log(arr)
+
+//add at last 
+let arr1 = [12, 34, 56, 78, 90, 21];
+let b = arr1.push(20)
+console.log(arr1)
+
+//remove at start
+let arr2 = [5, 15, 25, 35, 45, 55];
+let c = arr2.shift()
+console.log(arr2)
+
+//add at start 
+
+let arr10 = [7, 14, 21, 28, 35, 42];
+let d = arr10.unshift(3090)
+console.log(arr10)
