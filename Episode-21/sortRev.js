ee3f55787168d346017bedcdea0806cd2f108eee
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2025-04-10 10:33:47 +0530

    sort-rev-string

---
+++ Episode-21/sortRev.js
@@ -0,0 +1,11 @@
+let arr1 = [9,3,12, 34, 56, 78, 90, 21,100,345,678,987,"&"," "];
+arr1.sort()//convert to string and sort by ascci order
+console.log(arr1)
+
+//reverse
+let arr2 = [5, 15, 25, 35, 45, 55];
+arr2.reverse()
+console.log(arr2)
+//includes
+let arr3 = [101, 202, 303, 404, 505, 606];
+console.log(arr2.includes(40))
---
+++ Episode-21/toString.js
@@ -0,0 +1,10 @@
+let arr4 = [9, 18, 27, 36, 45, 54];
+let joinVal = arr4.join("-")//parameter is separator
+console.log(joinVal)
+
+//to string 
+let arr5 = [87, 65, 43, 21, 10, 0];
+console.log(typeof(arr5))
+let stringVa = arr5.toString()
+console.log(stringVa)
+console.log(typeof(stringVa))//look it converts to string 
