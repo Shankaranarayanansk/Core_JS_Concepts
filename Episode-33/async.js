76c61d325d8969a416500aefee6d87e64597d47b
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2025-05-03 12:13:02 +0530

    CallStack

---
+++ Episode-33/async.js
@@ -0,0 +1,13 @@
+function f1(){
+    console.log("Naa f1")
+}
+function f2(){
+    console.log("Naa f2")
+}
+function f3(){
+    console.log("Naa f3")
+}
+f1()
+setTimeout(f2,2000)
+f3()
+//webapis - setTimeout()
